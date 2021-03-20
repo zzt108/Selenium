74afb332c5c3bd2be04541aeb97e6f086ac9dfbc
Zsolt Gershon Toth <zzt@outlook.hu>
2021-03-20 12:51:50 +0100

    ISelenium comment added

--- Selenium/Interfaces/ISelenium.cs
+++ Selenium/Interfaces/ISelenium.cs
@@ -6,6 +6,9 @@ namespace Selenium.Interfaces
 {
 	public interface ISelenium
 	{
+        /// <summary>
+        /// The base url to which all requested pages are relative
+        /// </summary>
 		string BaseUrl { get; }
 		IWebDriver Driver { get; }
 		void Quit();
